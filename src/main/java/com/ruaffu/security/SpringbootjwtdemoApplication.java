3f8b0c2ad898a328186d4063c481ace7f4381521
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2023-01-17 17:13:24 +0100

    initial commit

---
+++ src/main/java/com/ruaffu/security/SpringbootjwtdemoApplication.java
@@ -0,0 +1,15 @@
+package com.ruaffu.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjwtdemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootjwtdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruaffu/security/SpringbootjwtdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ruaffu.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjwtdemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
